d703b71a76de9a159c99db43702856cbeabadaee
parag-1812 <paraggupta9988@gmail.com>
2024-11-29 04:18:29 +0530

    Implemented ExpenseDAO class with addExpense and getAllExpenses methods

--- src/main/util/DatabaseUtil.java
+++ src/main/util/DatabaseUtil.java
@@ -5,11 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/ExpenseTrackerDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root@18127023";
-
     public static Connection getConnection() throws SQLException {
+        String DB_URL = "jdbc:mysql://localhost:3306/ExpenseTrackerDB";
+        String USER = "root";
+        String PASSWORD = "root@18127023";
         return DriverManager.getConnection(DB_URL, USER, PASSWORD);
     }
 
